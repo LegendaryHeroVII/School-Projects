bcb47274d12b9d441b9fb72281ee14b7240126b3
LegendaryHeroVII <69889877+LegendaryHeroVII@users.noreply.github.com>
2020-08-19 12:54:35 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/**
+* Java Program Name
+*
+* Dalton Gramling
+* August 19, 2020
+* CMSC 255 - C90
+*/
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Dalton Gramling");
+		System.out.println("Introduction to the Course - Lab 1");
+		System.out.println("Learn the bare basics of Java with the iconic Hello World program");
+		System.out.println("08/19/2020");
+		System.out.println("CMSC 255 - C90");
+		System.out.println();
+	}
+}
